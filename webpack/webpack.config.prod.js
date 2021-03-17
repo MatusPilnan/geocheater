135b73a48fcce90354b97683e0624c7e4cbcfb40
MatusPilnan <m.pilnan@gmail.com>
2021-03-17 22:01:30 +0100

    add release pipeline

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -18,36 +18,35 @@ module.exports = _.merge({}, config, {
 
   // devtool: 'eval',
   plugins: [
-    new CopyWebpackPlugin([
-      { from: './src' }
-    ], {
-      ignore: ['js/**/*', 'manifest.json'],
-      copyUnmodified: true
-    }),
-    new VersionFilePlugin({
-      packageFile: path.resolve(__dirname, '../package.json'),
-      template: path.resolve(__dirname, '../src/manifest.json'),
-      outputFile: path.resolve(__dirname, '../build/prod/manifest.json'),
-    }),
-    new CrxPlugin({
-      keyFile: '../mykey.pem',
-      contentPath: '../build/prod',
-      outputPath: '../build',
-      name: appName
-    }),
+    new CopyWebpackPlugin(
+      { patterns: [
+          {
+            from: './src',
+            globOptions: {
+              ignore: ['js/**/*', 'manifest.json']
+            }
+          }
+        ]}
+    ),
+    // new CrxPlugin({
+    //   keyFile: '../mykey.pem',
+    //   contentPath: '../build/prod',
+    //   outputPath: '../build',
+    //   name: appName
+    // }),
     new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' }),
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        screw_ie8: true,
-        warnings: false
-      },
-      mangle: {
-        screw_ie8: true
-      },
-      output: {
-        comments: false,
-        screw_ie8: true
-      }
-    }),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compressor: {
+    //     screw_ie8: true,
+    //     warnings: false
+    //   },
+    //   mangle: {
+    //     screw_ie8: true
+    //   },
+    //   output: {
+    //     comments: false,
+    //     screw_ie8: true
+    //   }
+    // }),
   ]
 });
